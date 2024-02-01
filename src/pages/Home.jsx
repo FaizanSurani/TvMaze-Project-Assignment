fa0653aff3617a15c64d1977a694baa1d2cb55c8
Faizan-Surani <suranifaizan52@gmail.com>
2024-02-01 15:16:35 +0530

    Complete the MovieDetails Page and Create BookingForm

---
+++ src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await fetch("https://api.tvmaze.com/search/shows?q=avengers");
+      const data = await res.json();
+      setMovies(data);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <div className="relative">
+        <h1 className="text-6xl mb-6 px-7 py-3 text-center mt-6 border-2 border-black shadow-md">
+          Movie List
+        </h1>
+        <div className="px-4 py-2 mt-2 rounded shadow-lg text-center">
+          <ul>
+            {movies.map((movie) => (
+              <li
+                key={movie.show.id}
+                className="border border-gray-400 mb-6 shadow-lg bg-gray-200">
+                <Link to={`/movie/${movie.show.id}`}>
+                  <h3 className="font-bold text-2xl mt-6 mb-6">
+                    {movie.show.name}
+                  </h3>
+                </Link>
+                <div className="mb-6">
+                  <h2 className="text-blue-700 hover:text-blue-800 cursor-pointer mb-2">
+                    {movie.show.url}
+                  </h2>
+                  <div className="flex justify-center items-center ">
+                    <h2 className="font-serif text-lg mr-6">
+                      {`Runtime : ${movie.show.averageRuntime} mins`}
+                    </h2>
+                    <h2 className="font-mono text-lg">{`Premiered On : ${movie.show.premiered}`}</h2>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function MovieDetails() {
+  const { id } = useParams();
+  const [moviesDetails, setMoviesDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+      const data = await res.json();
+      setMoviesDetails([data]);
+    };
+    fetchMovies();
+  }, [id]);
+  return (
+    <>
+      <div>
+        <h2 className="text-6xl mb-6 px-7 py-3 text-center mt-6 border-2 border-black shadow-md bg-gray-700 text-white rounded">
+          Movie Details
+        </h2>
+        <div>
+          {moviesDetails.map((movie) => (
+            <ul>
+              <h3 className="text-3xl mb-6 text-center px-7 py-3 shadow-2xl rounded">
+                {movie.name}
+              </h3>
+              <li
+                key={movie.id}
+                className="mt-6 mb-6 text-3xl text-center px-7 py-3">
+                {movie.summary}
+              </li>
+              <div className="flex justify-center items-center flex-col">
+                <h3 className="font-serif text-lg mr-6">
+                  {`Runtime : ${movie.averageRuntime} mins`}
+                </h3>
+                <h3 className="font-mono text-lg">{`Premiered On : ${movie.premiered}`}</h3>
+              </div>
+            </ul>
+          ))}
+        </div>
+        <div className="mt-6 flex items-center justify-center">
+          <Link to={`/booking/${id}`}>
+            <button className="px-7 py-3 text-white bg-blue-600 w-full rounded uppercase font-semibold transition duration-150 ease-in-out hover:bg-blue-700 active:bg-blue-800">
+              Book Ticket
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
